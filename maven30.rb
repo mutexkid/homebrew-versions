5bcead6362f0f469e1dcffc651ced49a20182675
Josh Skeen <josh@joshskeen.com>
2013-10-30 16:57:18 -0400

    hardcode maven path

--- maven30.rb
+++ maven30.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Maven30 < Formula
   homepage 'http://maven.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz'
+  url 'http://apache.mirrors.lucidnetworks.net/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz'
   sha1 'aecc0d3d67732939c0056d4a0d8510483ee1167e'
 
   def install
